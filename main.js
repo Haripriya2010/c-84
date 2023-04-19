01aec8a10c6f0c29555c7a2c60f071d27a150026
Haripriya2010 <113229964+Haripriya2010@users.noreply.github.com>
2023-04-19 13:13:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,92 @@
+canvas=document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+rover_width = 100;
+rover_height= 90;
+rover_x =10;
+rover_y = 100;
+
+images_array=["img 1.jpg","img 2.jpg","img 3.jpg" ,"img 4.jpg"];
+random_num=Math.floor(Math.random ()*4);
+
+background_image = images_array[random_num];
+rover_image = "rover.png";
+
+function add()
+ { 
+    background_imgTag = new Image(); //defining a variable with a new image
+     background_imgTag.onload = uploadBackground; // setting a function, onloading this variable
+     background_imgTag.src = background_image; // load image
+
+      rover_imgTag = new Image(); //defining a variable with a new image
+       rover_imgTag.onload = uploadrover; // setting a function, onloading this variable
+        rover_imgTag.src = rover_image; // load image 
+    }
+function uploadBackground(){
+    ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
+
+}
+
+function  uploadrover(){
+    ctx.drawImage(rover_imgTag,rover_x,rover_y,rover_width,rover_height);
+}
+
+window.addEventListener("keydown",my_keydown);
+function  my_keydown(e){
+    keypressed=e.keyCode;
+    if(keypressed=="38"){
+        up();
+        console.log("up arrow key is presed");
+    }
+    if(keypressed=="40"){
+        down();
+        console.log("down arrow key is presed");
+    }
+    if(keypressed=="37"){
+        left();
+        console.log("left arrow key is presed");
+    }
+    if(keypressed=="39"){
+    right();
+        console.log("right arrow key is presed");
+    }
+
+}
+function up(){
+    if (rover_y > 0 ){
+        rover_y=rover_y-10;
+        console.log("when up arrow is pressed ,x="+ rover_x +" and y = " + rover_y);
+        uploadBackground();
+        uploadrover();
+
+    }
+}
+
+function down(){
+    if (rover_y < 500 ){
+        rover_y=rover_y+10;
+        console.log("when up arrow is pressed ,x="+ rover_x +" and y = " + rover_y);
+        uploadBackground();
+        uploadrover();
+
+    }
+}
+function left(){
+    if (rover_x > 0 ){
+        rover_x=rover_x-10;
+        console.log("when up arrow is pressed ,x="+ rover_x +" and y = " + rover_y);
+        uploadBackground();
+        uploadrover();
+
+    }
+}
+
+function right(){
+    if (rover_x < 700 ){
+        rover_x=rover_x + 10;
+        console.log("when up arrow is pressed ,x="+ rover_x +" and y = " + rover_y);
+        uploadBackground();
+        uploadrover();
+
+    }
+}
+
